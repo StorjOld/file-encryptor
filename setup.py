b1fd5769af9ff0f48a5efd0b02cdc5124016b1fd
Hugo Peixoto <hugo.peixoto@gmail.com>
2014-04-27 03:28:25 +0100

    Adds package setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+    name='file_encryptor',
+    version='0.1.0',
+    author='Hugo Peixoto',
+    author_email='hugo.peixoto@gmail.com',
+    packages=['file_encryptor'],
+    scripts=[],
+    url='https://github.com/Storj/file-encryptor',
+    license='LICENSE',
+    description='Convergent encryption, focused on file handling',
+    long_description=open('README.md').read(),
+    install_requires=[
+        'pycryptopp >= 0.6.0',
+    ],
+)
