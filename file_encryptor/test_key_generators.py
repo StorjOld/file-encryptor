f0e45819eec7dd3cdf56a68ea585bd18733ec1b3
Josh Brandoff <josh@Emergent-Gestalt.local>
2014-09-10 00:55:24 -0400

    Explicitly cast strings to bytes before writing test strings to files

--- file_encryptor/test_key_generators.py
+++ file_encryptor/test_key_generators.py
@@ -13,13 +13,13 @@ class TestKeyGenerators(unittest.TestCase):
         self.sample3 = os.path.join(self.directory, "frowny.txt")
 
         with open(self.sample1, "wb") as f:
-            f.write("Superstar!\n")
+            f.write("Superstar!\n".encode('utf-8'))
 
         with open(self.sample2, "wb") as f:
-            f.write("Superstar!\n")
+            f.write("Superstar!\n".encode('utf-8'))
 
         with open(self.sample3, "wb") as f:
-            f.write("Frowny face :(\n")
+            f.write("Frowny face :(\n".encode('utf-8'))
 
     def tearDown(self):
         os.remove(self.sample1)
