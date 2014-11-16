0c98a7365421aea38629e36315f6e68447df8896
Josh Brandoff <josh@joshua.is>
2014-11-15 21:40:33 -0500

    Spruce up setup.py, make more completed and use PyTest instead.

--- setup.py
+++ setup.py
@@ -1,22 +1,74 @@
-from distutils.core import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-try:
-    longdescription = open('README.md').read()
-except:
-    longdescription = 'Convergent encryption, focused on file handling'
+# The MIT License (MIT)
+#
+# Copyright (c) 2014 Storj Labs
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import setup
+from setuptools.command.test import test as TestCommand
+import file_encryptor
+
+install_requirements = [
+    'pycrypto>=2.6.1'
+]
+
+test_requirements = [
+    'pytest',
+    'coverage',
+    'pytest-pep8',
+    'pytest-cache',
+    'coveralls'
+]
+
+
+class PyTest(TestCommand):
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Import PyTest here because outside, the eggs are not loaded.
+        import pytest
+        import sys
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 setup(
     name='file_encryptor',
-    version='0.1.0',
-    author='Hugo Peixoto',
-    author_email='hugo.peixoto@gmail.com',
-    packages=['file_encryptor'],
-    scripts=[],
+    version=file_encryptor.__version__,
     url='https://github.com/Storj/file-encryptor',
-    license='LICENSE',
-    description='Convergent encryption, focused on file handling',
-    long_description=longdescription,
-    install_requires=[
-        'pycrypto >= 2.6.1',
-    ]
+    license='MIT',
+    author='Storj Labs',
+    author_email='hello@storj.io',
+    description='Tool for convergently encryption files used by MetaDisk.',
+    packages=['file_encryptor'],
+    cmdclass={'test': PyTest},
+    install_requires=install_requirements,
+    tests_require=test_requirements
 )
